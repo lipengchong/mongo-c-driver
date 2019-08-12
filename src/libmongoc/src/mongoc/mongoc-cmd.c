76f6ee6c3187658d0985c58a2966aa7b01e786df
Isabel Atkinson <isabelatkinson@gmail.com>
2019-08-12 11:50:19 -0400

    fixed find bug

--- src/libmongoc/src/mongoc/mongoc-cmd.c
+++ src/libmongoc/src/mongoc/mongoc-cmd.c
@@ -757,12 +757,6 @@ _is_retryable_read (const mongoc_cmd_parts_t *parts,
       return false;
    }
 
-   if (mongoc_uri_get_option_as_bool (parts->client->uri,
-                                      MONGOC_URI_RETRYREADS,
-                                      !MONGOC_DEFAULT_RETRYREADS)) {
-      return false;
-   }
-
    return true;
 }
 
